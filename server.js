026d74fb030a3f52313966d117874f11e66cdb0c
Aman Batra <38990408+mnbatra@users.noreply.github.com>
2021-04-29 03:35:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -51,5 +51,9 @@ app.use("/api/users", users);
 app.use("/api/foodEntries", foodTracker)
 app.use("/api/feedback", feedback)
 
+//files
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
